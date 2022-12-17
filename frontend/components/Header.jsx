ce97815de1edd7096d55bf5b3ce5b18ec6fc0acf
Harshavardhana-B-S <harshabs113@gmail.com>
2022-12-17 12:43:09 +0530

    added header

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -1,22 +1,31 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-   <>
-   <div className='Banner'>
-       Lorem ipsum dolor sit amet consectetur adipisicing.
-   </div>
-   
-   <div className='navBarContainer'>
+    <>
+      <div className='Banner'>
+        Lorem ipsum dolor sit amet consectetur adipisicing.
+      </div>
 
-    <div className='navBarLogo'>
-        <img src="" alt="" />
-        <h2>Hello world</h2>
-    </div>
+      <div className='navBarContainer'>
 
-   </div>
-   
-   </>
+        <div className='navBarLogo'>
+          <img src="./assests/logo50.png" alt="" className='bg-[#5645bd]' />
+          <h2>Business Gram</h2>
+        </div>
+
+
+        <div className='navLinks'>
+          <button className='link'>Home</button>
+          <button className='link'>Business</button>
+          <button className='link'>Add Business</button>
+          <button className='link'>Contact</button>
+        </div>
+
+      </div>
+
+    </>
   )
 }
 
