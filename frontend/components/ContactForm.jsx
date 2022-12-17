42126e2660b6f21cf1a0475f7475297f05d249f6
shreyas0924 <shreyasp0924@gmail.com>
2022-12-17 12:44:28 +0530

    updated contact form

--- frontend/components/ContactForm.jsx
+++ frontend/components/ContactForm.jsx
@@ -13,13 +13,13 @@ export default function ContactForm() {
 
   return (
     <div>
-      <main className='  h-screen items-center flex justify-center'>
+      <main className='  h-screen items-center flex justify-center  '>
         <form
           onSubmit={formik.handleSubmit}
-          className='bg-white flex rounded-lg w-1/2 shadow-lg shadow-gray-300 '
+          className='bg-white flex  w-1/2 border-white border-2  '
         >
           <div className='flex-1 text-gray-700  p-20'>
-            <h1 className='text-4xl pb-2 font-bold'>Get Help with 'our name' 👋</h1>
+            <h1 className='text-4xl pb-2 font-bold'>Get Help with 'our name' </h1>
             <p className='text-black-50 mt-1 '>Share any problem, feedback, or any questions you have.</p>
             
             <div className='mt-6 '>
@@ -42,7 +42,7 @@ export default function ContactForm() {
                   className='border-2 border-gray-500 p-2 rounded-md w-1/2'
                   type='text'
                   name='name'
-                  placeholder='Enter your name'
+                  
                   onChange={formik.handleChange}
                   value={formik.values.name}
                   onBlur={formik.handleBlur}
@@ -67,38 +67,39 @@ export default function ContactForm() {
                   className='border-2 border-gray-500 p-2 rounded-md w-1/2'
                   type='email'
                   name='email'
-                  placeholder='Enter your email address'
+                  
                   onChange={formik.handleChange}
                   value={formik.values.email}
                   onBlur={formik.handleBlur}
                 />
               </div>
-              {/* Subject input field */}
+              {/* Message input field */}
               <div className='pb-4'>
                 <label
-                  htmlFor='subject'
-                  className='block font-bold text-sm pb-2'
+                  htmlFor='text'
+                  className={`block font-bold text-sm pb-2 `}
                 >
-                  Subject
+                  Message
                 </label>
+
                 <input
                   className='border-2 border-gray-500 p-2 rounded-md w-1/2'
                   type='text'
-                  name='subject'
-                  placeholder='Enter message'
+                  name='message'
+                  
                   onChange={formik.handleChange}
-                  value={formik.values.email}
+                  value={formik.values.message}
                   onBlur={formik.handleBlur}
                 />
-                
               </div>
-              {/* Terms of service*/}
+              
+              
               
               <button
                 type='submit'
                 className='bg-[#3AACD9] text-sm text-white py-3 mt-6 rounded-lg w-full'
               >
-                Start learning today!
+                Submit 
               </button>
             </div>
           </div>
