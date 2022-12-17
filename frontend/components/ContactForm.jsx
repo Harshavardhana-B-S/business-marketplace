b676630651e4e42de36b4b686ee49c3d42749926
shreyas0924 <shreyasp0924@gmail.com>
2022-12-17 15:22:29 +0530

    updated contact form

--- frontend/components/ContactForm.jsx
+++ frontend/components/ContactForm.jsx
@@ -1,21 +1,33 @@
-import React from 'react'
-import { useFormik } from 'formik'
+import React, { useState } from 'react'
+
 
 export default function ContactForm() {
-  const formik = useFormik({
-    initialValues: {
-      name: '',
-      email: '',
-      country: '',
-      terms: '',
-    },
-  })
+  
+
+  const [name,setName] = useState('')
+  const [email,setEmail] = useState('')
+  const [message,setMessage] = useState('')
+
+
+  async function registerUser(event) {
+    event.preventDefault()
+    const response = await fetch('http://localhost:1337/api/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ name, email, message }),
+    })
+
+    const data = await response.json()
+    console.log(data)
+  }
 
   return (
     <div>
       <main className='  h-screen items-center flex justify-center  '>
         <form
-          onSubmit={formik.handleSubmit}
+          onSubmit={registerUser}
           className='bg-white flex  w-1/2 border-white border-2  '
         >
           <div className='flex-1 text-gray-700  p-20'>
@@ -23,57 +35,42 @@ export default function ContactForm() {
             <p className='text-black-50 mt-1 '>Share any problem, feedback, or any questions you have.</p>
             
             <div className='mt-6 '>
-              {/* Name input field */}
+              
               <div className='pb-4'>
                 <label
                   htmlFor='name'
-                  className={`block font-bold text-sm pb-2 ${
-                    formik.touched.name && formik.errors.name
-                      ? 'text-red-400'
-                      : ''
-                  } `}
+                  className={`block font-bold text-sm pb-2`}
                 >
-                  {formik.touched.name && formik.errors.name
-                    ? formik.errors.name
-                    : 'Name'}
+                Name
                 </label>
                 <p className='text-sm font-bold text-red-400 '></p>
                 <input
                   className='border-2 border-gray-500 p-2 rounded-md w-1/2'
                   type='text'
                   name='name'
+                 
+                  value={name}
                   
-                  onChange={formik.handleChange}
-                  value={formik.values.name}
-                  onBlur={formik.handleBlur}
+                  onChange={(e) => setName(e.target.value)}
                 />
               </div>
-              {/* Email input field */}
+              
               <div className='pb-4'>
                 <label
                   htmlFor='email'
-                  className={`block font-bold text-sm pb-2 ${
-                    formik.touched.email && formik.errors.email
-                      ? 'text-red-400'
-                      : ''
-                  }`}
-                >
-                  {formik.touched.email && formik.errors.email
-                    ? formik.errors.email
-                    : 'Email'}
+                  className={`block font-bold text-sm pb-2 `} >
+                  Email
                 </label>
 
                 <input
                   className='border-2 border-gray-500 p-2 rounded-md w-1/2'
                   type='email'
                   name='email'
-                  
-                  onChange={formik.handleChange}
-                  value={formik.values.email}
-                  onBlur={formik.handleBlur}
+                  onChange={(e) => setEmail(e.target.value)}
+                  value={email}                 
                 />
               </div>
-              {/* Message input field */}
+              
               <div className='pb-4'>
                 <label
                   htmlFor='text'
@@ -87,9 +84,9 @@ export default function ContactForm() {
                   type='text'
                   name='message'
                   
-                  onChange={formik.handleChange}
-                  value={formik.values.message}
-                  onBlur={formik.handleBlur}
+                  onChange={(e) => setMessage(e.target.value)}
+                  value={message}
+                 
                 />
               </div>
               
