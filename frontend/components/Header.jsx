d35dde03d5b3e82b8f0b729f341348745f2ba592
shreyas0924 <shreyasp0924@gmail.com>
2022-12-17 11:22:00 +0530

    frontend setup

---
+++ frontend/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className=''>
+      <h1 className='text-large text-center'>hello</h1>
+    </div>
+  )
+}
+
+export default Header
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+
+import Header from '../components/Header'
+import './index.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Header />
+      {/* <About />
+      <Footer /> */}
      </div>
   )
 }
