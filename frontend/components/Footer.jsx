561ff81b8797af8beea0639f255fb9f8d37489aa
Harshavardhana-B-S <harshabs113@gmail.com>
2022-12-17 19:30:14 +0530

    update: interval push

--- frontend/components/Footer.jsx
+++ frontend/components/Footer.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 function Footer()  {
   return (
-    <div>Footer</div>
+    <>
+      <div className='footer'>
+        <hr />
+
+      <img src="./assests/logo50.png" alt="" className='bg-[#5645bd]' />
+
+      <div className='footerDetails'>
+            <h3>Resources</h3>      
+            
+      </div>
+        
+      </div>
+       
+      
+    </>
   )
 }
 
